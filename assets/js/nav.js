8dd429407a3fa5184e46af13e0c211ef08fcb474
Annie Liu <aliu23@calpoly.edu>
2019-01-21 19:36:17 -0800

    adjusted nav underline values

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,7 +1,7 @@
 function underlineNav() {
    var nav_width = document.getElementById("main-nav").offsetWidth;
-   var text_length_total = 91 + 125 + 68 + 91;
-   var text_padding = (nav_width - text_length_total) / 8;
+   var text_length_total = 91 + 120 + 91;
+   var text_padding = (nav_width - text_length_total) / 6;
 
    var location = window.location.pathname;
    var underline = document.getElementById("nav-underline");
@@ -12,16 +12,12 @@ function underlineNav() {
       underline.style.background = "#ea3379";
    } else if (location == "/portfolio/") {
       underline.style.marginLeft = (text_padding * 3 + 91) + "px";
-      underline.style.width = "125px";
+      underline.style.width = "120px";
       underline.style.background = "#66aaff";
-   } else if (location == "/blog/") {
-      underline.style.marginLeft = (text_padding * 5 + 91 + 125) + "px";
-      underline.style.width = "68px";
-      underline.style.background = "#32ffa6";
    } else if (location == "/about/") {
-      underline.style.marginLeft = (text_padding * 7 + 91 + 125 + 68) + "px";
+      underline.style.marginLeft = (text_padding * 5 + 91 + 120) + "px";
       underline.style.width = "91px";
-      underline.style.background = "tomato";
+      underline.style.background = "#32ffa6";
    }
 };
 
@@ -29,8 +25,9 @@ underlineNav();
 
 function underlineHome() {
    var nav_width = document.getElementById("main-nav").offsetWidth;
-   var text_length_total = 91 + 125 + 68 + 91;
-   var text_padding = (nav_width - text_length_total) / 8;
+
+   var text_length_total = 91 + 120 + 91;
+   var text_padding = (nav_width - text_length_total) / 6;
    var underline = document.getElementById("nav-underline");
    
    underline.style.marginLeft = text_padding + "px";
@@ -40,33 +37,22 @@ function underlineHome() {
 
 function underlinePortfolio() {
    var nav_width = document.getElementById("main-nav").offsetWidth;
-   var text_length_total = 91 + 125 + 68 + 91;
-   var text_padding = (nav_width - text_length_total) / 8;
+   var text_length_total = 91 + 120 + 91;
+   var text_padding = (nav_width - text_length_total) / 6;
    var underline = document.getElementById("nav-underline");
 
    underline.style.marginLeft = (text_padding * 3 + 91) + "px";
-   underline.style.width = "125px";
+   underline.style.width = "120px";
    underline.style.background = "#66aaff";
-}
-
-function underlineBlog() {
-   var nav_width = document.getElementById("main-nav").offsetWidth;
-   var text_length_total = 91 + 125 + 68 + 91;
-   var text_padding = (nav_width - text_length_total) / 8;
-   var underline = document.getElementById("nav-underline");
-
-   underline.style.marginLeft = (text_padding * 5 + 91 + 125) + "px";
-   underline.style.width = "68px";
-   underline.style.background = "#32ffa6";
 }
 
 function underlineAbout() {
    var nav_width = document.getElementById("main-nav").offsetWidth;
-   var text_length_total = 91 + 125 + 68 + 91;
-   var text_padding = (nav_width - text_length_total) / 8;
+   var text_length_total = 91 + 120 + 91;
+   var text_padding = (nav_width - text_length_total) / 6;
    var underline = document.getElementById("nav-underline");
 
-   underline.style.marginLeft = (text_padding * 7 + 91 + 125 + 68) + "px";
+   underline.style.marginLeft = (text_padding * 5 + 91 + 120) + "px";
    underline.style.width = "91px";
-   underline.style.background = "tomato";
+   underline.style.background = "#32ffa6";
 }
